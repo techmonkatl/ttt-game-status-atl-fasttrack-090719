39a56891ade0431fc6db7acb59e521fcea04292a
Bronze Law <bronzelaw3@gmail.com>
2019-09-08 16:05:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,5 +1,9 @@
-# Helper Method
-def position_taken?(board, index)
+board = [" ", " ", " ", " ", " "," "," "," ", " "]
+def WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5]  # Middle row
+  # ETC, an array for each win combination
+]position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
